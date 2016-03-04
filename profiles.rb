a9712078c9c8793fcf2361a3e50e835ee2e9b4e5
Chris Heald <cheald@gmail.com>
2016-03-03 17:44:10 -0700

    Fix root-level output for KiTTY

--- profiles.rb
+++ profiles.rb
@@ -132,7 +132,7 @@ module AWSProfileGenerator
 
     class KiTTY < Base
       def setup
-        @structure = @config.fetch("structure", "").split(/[\\\/:]/)
+        @structure = ["_root"] + @config.fetch("structure", "").split(/[\\\/:]/)
         @basepath = File.expand_path @config["directory"]
         FileUtils.mkdir_p @basepath
         @template = ERB.new open(@config["template"]).read
@@ -155,6 +155,7 @@ module AWSProfileGenerator
 
       def files_for(instance)
         permute instance, @structure, true do |f|
+          f.shift
           dir = File.join(@basepath, *f)
           FileUtils.mkdir_p(dir)
           yield ordinal_name_for File.join(dir, encode(instance.name))
@@ -165,7 +166,13 @@ module AWSProfileGenerator
         return nil if keys.empty?
         keys = keys.dup
         key = keys.shift
-        vals = (instance.tag(key) || (instance.respond_to?(key) ? instance.send(key) : "")).split(",").map(&:strip)
+        vals = if key.match(/^\$(.*)/)
+          key = key.gsub(/^\$/, "")
+          (instance.tag(key) || (instance.respond_to?(key) ? instance.send(key) : "")).split(",").map(&:strip)
+        else
+          [key]
+        end
+
         vals.map do |val|
           result = [val, permute(instance, keys)]
           yield result.flatten.compact if top
